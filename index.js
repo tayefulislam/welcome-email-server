46094a1cb8f29a103a2b4a1f790368357786daa5
tayeful912 <kazitayefulislam12@gmail.com>
2022-06-01 17:29:54 +0600

    basic server setup

---
+++ index.js
@@ -0,0 +1,19 @@
+const express = require("express")
+const cors = require("cors")
+require('dotenv').config()
+const app = express()
+const port = process.env.PORT || 5000;
+
+
+//middleware
+
+app.use(cors())
+app.use(express.json())
+
+app.get('/', (req, res) => {
+    res.send('Welcome Email Server')
+})
+
+app.listen(port, () => {
+    console.log(`Welcome Email Port Server`, port)
+})
